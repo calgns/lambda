9f14935f4a3e725c238ec6dec5e9dd939701da2f
Matheus Henrique <matheus110903@gmail.com>
2021-08-15 12:46:42 -0300

    terminado lambda e comentarios

--- lambida.py
+++ lambida.py
@@ -2,12 +2,26 @@
 # a lambida é boa por usar 1 linha
 # e deixar as coisas mais "simples".
 # exemplo:
+# usando a função def:
+print("def:")
 def soma(a,b):
      print(a + b)
 soma(9,6)
-
+# multiplica.
 def mult(a,b):
     vezes = a * b
-    print(vezes)
-    
-mult(12,9)
+    return vezes
+print(mult(12,9))
+# agora usando a lambda:
+# soma.
+print("\nlambda:")
+soma2 = lambda x,y:x+y
+print(soma2(2,2))
+# multiplica.
+mult2=lambda x,y:print(x*y)
+mult2(2,11)
+divi = lambda d,o: d/o
+print(divi(15,5))
+print(divi(10,2))
+# ....
+# por isso a lambida é melhor por ser mais curta e ate rapida

5999b6a4801880523cb44ca5ebd1951abe979702
Matheus Henrique <matheus110903@gmail.com>
2021-08-15 12:33:29 -0300

    definindo funçoes

--- lambida.py
+++ lambida.py
@@ -2,3 +2,12 @@
 # a lambida é boa por usar 1 linha
 # e deixar as coisas mais "simples".
 # exemplo:
+def soma(a,b):
+     print(a + b)
+soma(9,6)
+
+def mult(a,b):
+    vezes = a * b
+    print(vezes)
+    
+mult(12,9)
